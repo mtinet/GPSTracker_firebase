cf8dc4d8eee3b19c761bf330fbd2d4dd5573d513
Jason Juhyun Kim <mtinet@hanmail.net>
2021-02-01 08:37:50 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -92,6 +92,10 @@ rootRef.on('child_changed', snapshot => {
   console.log('child(s)_changed!');
 })
 
+database.ref('/communities').orderByValue().on('value', snapshot => {
+  console.log(snapshot.val());
+})
+
 // 자식 노드가  변화될 때마다 콘솔에 표시
 // rootRef.on('value', snapshot => {
 //   console.log('데이터베이스에 이벤트가 발생했습니다.');
@@ -102,47 +106,47 @@ rootRef.on('child_changed', snapshot => {
 //   console.log('데이터베이스에 이벤트가 발생했습니다.');
 // })
 
-// 특정 자식 노드가 추가, 업데이트, 삭제되면 콘솔에 표시
-rootRef.on('child_changed', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리
-rootRef.orderByKey().on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 키의 순서로 앞에서 2개만 불러오기
-rootRef.orderByKey().limitToFirst(2).on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 키의 순서로 마지막 2개만 불러오기
-rootRef.orderByKey().limitToLast(2).on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 자식 노드를 지정하여 정렬 후 불러오기
-rootRef.orderByChild('age').limitToLast(1).on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 자식 노드를 지정한 다음 지정하는 값(여기서는 'john')과 똑같은 것을 불러오기
-rootRef.orderByChild('name').equalTo('john').on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 자식 노드를 지정한 다음 시작값이 같은 값(여기서는 'c')과 똑같은 것을 불러오기
-rootRef.orderByChild('name').startAt('c').on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 값을 지정한 다음 불러오기
-database.ref('/communities').orderByValue().on('value', snapshot => {
-  console.log(snapshot.val());
-})
-
-// 쿼리, 값을 지정한 다음 정렬 후 앞의 두 값 불러오기
-database.ref('/communities').orderByValue().limitToFirst(2).on('value', snapshot => {
-  console.log(snapshot.val());
-})
+// // 특정 자식 노드가 추가, 업데이트, 삭제되면 콘솔에 표시
+// rootRef.on('child_changed', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리
+// rootRef.orderByKey().on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 키의 순서로 앞에서 2개만 불러오기
+// rootRef.orderByKey().limitToFirst(2).on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 키의 순서로 마지막 2개만 불러오기
+// rootRef.orderByKey().limitToLast(2).on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 자식 노드를 지정하여 정렬 후 불러오기
+// rootRef.orderByChild('age').limitToLast(1).on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 자식 노드를 지정한 다음 지정하는 값(여기서는 'john')과 똑같은 것을 불러오기
+// rootRef.orderByChild('name').equalTo('john').on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 자식 노드를 지정한 다음 시작값이 같은 값(여기서는 'c')과 똑같은 것을 불러오기
+// rootRef.orderByChild('name').startAt('c').on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 값을 지정한 다음 불러오기
+// database.ref('/communities').orderByValue().on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
+// 
+// // 쿼리, 값을 지정한 다음 정렬 후 앞의 두 값 불러오기
+// database.ref('/communities').orderByValue().limitToFirst(2).on('value', snapshot => {
+//   console.log(snapshot.val());
+// })
