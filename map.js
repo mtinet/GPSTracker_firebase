02b10810a110d7a220146610033bbd39313745d6
Jason Juhyun Kim <mtinet@hanmail.net>
2021-02-15 11:51:16 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,39 +1,48 @@
 // firebase 데이터 가져오기
 const database = firebase.database();
-var lat = 0;
-var long = 0;
+var locationVal = database.ref('/gpsFirebase')
+locationVal.on('value', gotData, errData);
 
-// 가독성을 위해 변수.child를 이용하도록 세팅
-const rootRef = database.ref('users');
+function gotData(data) {
+  var data = data.val();
+  //var keys = Object.keys(data);
 
-database.ref('/gpsFirebase/lat').on('value', function(snapshot) {
-  console.log(snapshot.val());
-})
+  // console.log(data.val());
+  console.log(data.lat);
+  console.log(data.long);
 
-database.ref('/gpsFirebase/long').on('value', snapshot => {
-  console.log(snapshot.val());
-})
+  var lat = data.lat;
+  var long = data.long;
 
+  document.getElementById("lat").innerHTML=lat;
+  document.getElementById("long").innerHTML=long;
 
-// 지도, 마커 가져오기
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-        center: new kakao.maps.LatLng(37.50701, 127.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+  // 지도, 마커 가져오기
+  var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+      mapOption = {
+          center: new kakao.maps.LatLng(lat, long), // 지도의 중심좌표
+          level: 3 // 지도의 확대 레벨
+      };
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+  var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// 마커가 표시될 위치입니다
-var markerPosition  = new kakao.maps.LatLng(37.50701, 127.570667);
+  // 마커가 표시될 위치입니다
+  var markerPosition  = new kakao.maps.LatLng(lat, long);
 
-// 마커를 생성합니다
-var marker = new kakao.maps.Marker({
-    position: markerPosition
-});
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+      position: markerPosition
+  });
 
-// 마커가 지도 위에 표시되도록 설정합니다
-marker.setMap(map);
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
 
-// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-// marker.setMap(null);
+  // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+  // marker.setMap(null);
+
+}
+
+function errData(data) {
+  console.log('Error!');
+  console.log(err);
+}
