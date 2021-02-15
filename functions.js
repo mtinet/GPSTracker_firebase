77301fbf909d22266888c9beb951df35433de4df
Jason Juhyun Kim <mtinet@hanmail.net>
2021-02-15 15:50:39 +0900

    Add files via upload

--- functions.js
+++ functions.js
@@ -5,9 +5,9 @@ var long = 0;
 // 가독성을 위해 변수.child를 이용하도록 세팅
 const rootRef = database.ref('users');
 
-lat = database.ref('/gpsFirebase/lat').on('value', snapshot => {
+database.ref('/gpsFirebase/lat').on('value', snapshot => {
   console.log(snapshot.val());
 })
-long = database.ref('/gpsFirebase/long').on('value', snapshot => {
+database.ref('/gpsFirebase/long').on('value', snapshot => {
   console.log(snapshot.val());
 })
