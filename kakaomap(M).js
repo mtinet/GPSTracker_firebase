9ef1588c2ca368f529290e0f702a08d023047439
Jason Juhyun Kim <mtinet@hanmail.net>
2021-03-01 12:42:42 +0900

    Add files via upload

---
+++ kakaomap(M).js
@@ -0,0 +1,43 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커를 표시할 위치와 title 객체 배열입니다
+var positions = [
+    {
+        title: '자전거 1',
+        latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+    },
+    {
+        title: '자전거 2',
+        latlng: new kakao.maps.LatLng(33.450805, 126.574677)
+    },
+    {
+        title: '자전거 3',
+        latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+    }
+];
+
+// 마커 이미지의 이미지 주소입니다
+var imageSrc = "./dot.png";
+
+for (var i = 0; i < positions.length; i ++) {
+
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 24);
+
+    // 마커 이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage // 마커 이미지
+    });
+}
