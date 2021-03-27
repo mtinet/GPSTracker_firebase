06d6f2eba0ae559e60d2ef985e0bc1e152337414
Jason Juhyun Kim <mtinet@hanmail.net>
2021-03-27 12:27:38 +0900

    Add files via upload

--- google(GMP).js
+++ google(GMP).js
@@ -1,8 +1,7 @@
-
 function initMap() {
   // firebase 데이터 가져오기
   const database = firebase.database();
-  var locationVal = database.ref('/gpsFirebase/bicycle_1')
+  var locationVal = database.ref('/gpsFirebase')
   locationVal.on('value', gotData, errData);
 
   function gotData(data) {
@@ -11,47 +10,84 @@ function initMap() {
 
     //console.log(data);
     //console.log(keys);
-    console.log(data.lat);
-    console.log(data.long);
+    console.log(data.bicycle_1.lat);
+    console.log(data.bicycle_1.long);
 
-    var lat = data.lat;
-    var long = data.long;
+    var lat1 = data.bicycle_1.lat;
+    var long1 = data.bicycle_1.long;
 
 
-    document.getElementById("lat").innerHTML=lat;
-    document.getElementById("long").innerHTML=long;
+    document.getElementById("lat1").innerHTML=lat1;
+    document.getElementById("long1").innerHTML=long1;
 
-  var latlong = {}
-  latlong['lat'] = Number(lat);
-  latlong['lng'] = Number(long);
+    var latlong1 = {}
+    latlong1['lat'] = Number(lat1);
+    latlong1['lng'] = Number(long1);
 
-  console.log(latlong);
+    console.log(latlong1);
 
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: latlong,
-    zoom: 17
-  });
+    console.log(data.bicycle_2.lat);
+    console.log(data.bicycle_2.long);
 
-  var marker;
-  marker = new google.maps.Marker({
-    position: latlong,
-    //icon: [URL],
-    map: map
-  });
+    var lat2 = data.bicycle_2.lat;
+    var long2 = data.bicycle_2.long;
 
-  var infowindow = new google.maps.InfoWindow();
-  var info = '우리집';
-  google.maps.event.addListener(marker, 'click', function(){
-    infowindow.setContent(info);
-    infowindow.open(map, marker);
-  })
 
-  if(marker){
-    marker.addListener('click', function(){
-      map.setCenter(this.getPosition());
-      map.setZoom(17);
+    document.getElementById("lat2").innerHTML=lat2;
+    document.getElementById("long2").innerHTML=long2;
+
+    var latlong2 = {}
+    latlong2['lat'] = Number(lat2);
+    latlong2['lng'] = Number(long2);
+
+    console.log(latlong2);
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+      center: latlong1,
+      zoom: 14
+    });
+
+    var marker1;
+    marker1 = new google.maps.Marker({
+      position: latlong1,
+      //icon: [URL],
+      map: map
+    });
+
+    var marker2;
+    marker2 = new google.maps.Marker({
+      position: latlong2,
+      //icon: [URL],
+      map: map
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var info1 = '자전거1';
+      google.maps.event.addListener(marker1, 'click', function(){
+      infowindow.setContent(info1);
+      infowindow.open(map, marker1);
     })
-  }
+
+    if(marker1){
+      marker1.addListener('click', function(){
+        map.setCenter(this.getPosition());
+        map.setZoom(17);
+      })
+    }
+
+    var info2 = '자전거2';
+    google.maps.event.addListener(marker2, 'click', function(){
+      infowindow.setContent(info2);
+      infowindow.open(map, marker2);
+    })
+
+    if(marker2){
+      marker2.addListener('click', function(){
+        map.setCenter(this.getPosition());
+        map.setZoom(17);
+      })
+    }
+
   }
 
   function errData(data) {
